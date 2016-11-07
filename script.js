480d22b249d4c55aa90faeccecd110a79970d824
katya20 <kpanchenko21@gmail.com>
2016-11-07 08:54:11 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
 function logArgs(args) {
   var a = 1
   console.log(args)
-}
+};
 
 var functionString = logArgs.toString();
 
-var consoleLogExample = $("#ex 1");
+var consoleLogExample = $("#ex1");
 
 var formattedExample = $("<pre>");
 
 formattedExample.text(functionString);
 
 consoleLogExample.html(formattedExample);
+
