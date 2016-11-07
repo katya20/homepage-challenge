6f949b6da7b3e8d3e35e9b4a32b438cf00083cf4
katya20 <kpanchenko21@gmail.com>
2016-11-07 09:35:54 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -20,15 +20,15 @@ consoleLogExample.html(formattedExample);
 // objects
 donut = {name: "donut", picture:  "http://www.withsprinklesontop.net/wp-content/uploads/2012/01/DSC_0406x900.jpg", price: "5", rating: "10/10", weight: "0.5lb"}
 
-var functionString = donut.toString();
+var functionString1 = donut.toString();
 
-var consoleLogExample = $("#ex2");
+var consoleLogExample1 = $("#ex2");
 
-var formattedExample = $("<pre>");
+var formattedExample1 = $("<pre>");
 
-formattedExample.text(functionString);
+formattedExample1.text(functionString1);
 
-consoleLogExample.html(formattedExample);
+consoleLogExample1.html(formattedExample1);
 
 
 
