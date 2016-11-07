046ad18685c457c7e11479c0002fba64c1eaf7b2
katya20 <kpanchenko21@gmail.com>
2016-11-07 09:44:10 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -18,9 +18,13 @@ consoleLogExample.html(formattedExample);
 
 
 // objects
-donut = {name: "donut", picture:  "http://www.withsprinklesontop.net/wp-content/uploads/2012/01/DSC_0406x900.jpg", price: "5", rating: "10/10", weight: "0.5lb"}
+donut = {name: "donut", 
+         picture:  "http://www.withsprinklesontop.net/wp-content/uploads/2012/01/DSC_0406x900.jpg", 
+         price: "5", 
+         rating: "10/10", 
+         weight: "0.5lb"}
 
-var functionString1 = donut
+var functionString1 = JSON.stringify(donut)
 
 var consoleLogExample1 = $("#ex2");
 
@@ -30,6 +34,22 @@ formattedExample1.text(functionString1);
 
 consoleLogExample1.html(formattedExample1);
 
+// functions
+donut = {name: "donut", 
+         picture:  "http://www.withsprinklesontop.net/wp-content/uploads/2012/01/DSC_0406x900.jpg", 
+         price: "5", 
+         rating: "10/10", 
+         weight: "0.5lb"}
+
+var functionString1 = JSON.stringify(donut)
+
+var consoleLogExample1 = $("#ex2");
+
+var formattedExample1 = $("<pre>");
+
+formattedExample1.text(functionString1);
+
+consoleLogExample1.html(formattedExample1);
 
 
 
