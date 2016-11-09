440603b7e7ddb334cbeb50e6e6ac73f865d8ae1e
katya20 <kpanchenko21@gmail.com>
2016-11-09 09:14:41 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -67,4 +67,11 @@ formattedExample3.text(functionString3);
 
 consoleLogExample3.html(formattedExample3);
 
+// function within a funtion
+function get(call) {
+  setTimeout(function() {
+    callback(1)
+  }, 1000)
+}
+
 
