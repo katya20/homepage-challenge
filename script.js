036fb6b22923d48328301cdb94d2f42aeabad632
katya20 <kpanchenko21@gmail.com>
2016-12-06 09:35:05 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -18,21 +18,21 @@
 
 
 // objects
-donut = {name: "donut", 
-         picture:  "http://www.withsprinklesontop.net/wp-content/uploads/2012/01/DSC_0406x900.jpg", 
-         price: "5", 
-         rating: "10/10", 
-         weight: "0.5lb"}
-
-var functionString1 = JSON.stringify(donut)
-
-var consoleLogExample1 = $("#ex2");
-
-var formattedExample1 = $("<pre>");
-
-formattedExample1.text(functionString1);
-
-consoleLogExample1.html(formattedExample1);
+//donut = {name: "donut", 
+//         picture:  "http://www.withsprinklesontop.net/wp-content/uploads/2012/01/DSC_0406x900.jpg", 
+//         price: "5", 
+//         rating: "10/10", 
+//         weight: "0.5lb"}
+//
+//var functionString1 = JSON.stringify(donut)
+//
+//var consoleLogExample1 = $("#ex2");
+//
+//var formattedExample1 = $("<pre>");
+//
+//formattedExample1.text(functionString1);
+//
+//consoleLogExample1.html(formattedExample1);
 
 // functions
 function saySomething() {
