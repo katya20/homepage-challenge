db9ff9ea232afa42513c9500450d558cc07726d0
katya20 <kpanchenko21@gmail.com>
2016-12-06 09:32:38 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 
 // firebase example
-function getNotesForName(name) {  
-  onNewListItem(nameInput.val() + "/notes", function(note) {
-    $("#note-list").append("<div>" + note + "</div>")
-  })
-}
-
-var functionString = getNotesForName.toString();
-
-var consoleLogExample = $("#ex1");
-
-var formattedExample = $("<pre>");
-
-formattedExample.text(functionString);
-
-consoleLogExample.html(formattedExample);
+//function getNotesForName(name) {  
+//  onNewListItem(nameInput.val() + "/notes", function(note) {
+//    $("#note-list").append("<div>" + note + "</div>")
+//  })
+//}
+//
+//var functionString = getNotesForName.toString();
+//
+//var consoleLogExample = $("#ex1");
+//
+//var formattedExample = $("<pre>");
+//
+//formattedExample.text(functionString);
+//
+//consoleLogExample.html(formattedExample);
 
 
 // objects
